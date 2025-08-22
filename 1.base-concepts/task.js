9eb28f31962733704a43157beb8309161626e106
Anna Baraeva <pfeffa.hain@gmail.com>
2025-08-22 21:19:03 +0300

    task #1 has been solved

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,33 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b ** 2 - 4 * a * c;
+
+  if (d < 0) {
+    arr = [];
+  }
+
+  if (d === 0) {
+    arr = [-b / (2 * a)];
+  }
+
+  if (d > 0) {
+    arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = percent / 100;
+  let monthPercent = percent / 12;
+  let loanBody = amount - contribution;
+  let monthPayment =
+    loanBody *
+    (monthPercent + monthPercent / ((1 + monthPercent) ** countMonths - 1));
+
+  let sum = monthPayment * countMonths;
+  let sumRounded = sum.toFixed(2);
+
+  return Number(sumRounded);
 }
