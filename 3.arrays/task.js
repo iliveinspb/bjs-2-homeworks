315887de1bd0aabb2d5f2fafab0e972819708fc0
Anna Baraeva <pfeffa.hain@gmail.com>
2025-09-21 18:45:48 +0300

    task netology-code #3 solved

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
 function compareArrays(arr1, arr2) {
-  
+  return (
+    arr1.length === arr2.length &&
+    arr1.every((element, index) => element === arr2[index])
+  );
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let userAges = users
+    .filter((user) => user.gender === gender)
+    .map((user) => user.age);
+
+  if (userAges.length === 0) {
+    return 0;
+  }
+
+  const sum = userAges.reduce(function (acc, age) {
+    return acc + age;
+  }, 0);
+  return sum / userAges.length;
 }
