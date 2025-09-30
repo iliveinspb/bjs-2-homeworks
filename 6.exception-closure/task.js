050a94a6452c829137c8fc84c5b861caefa41074
Anna Baraeva <pfeffa.hain@gmail.com>
2025-09-30 23:42:33 +0300

    first part solved

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,15 @@
-﻿
+﻿function parseCount(value) {
+  const parsed = Number.parseFloat(value);
+  if (Number.isNaN(parsed)) {
+    throw new Error("Невалидное значение");
+  }
+  return parsed;
+}
+
+function validateCount(value) {
+  try {
+    return parseCount(value);
+  } catch (error) {
+    return error;
+  }
+}
